94d9458165c818235468d6c25b2519347f8a3dc2
antonio suarez <antoniosuarezrincon@gmail.com>
2022-06-05 15:07:37 -0500

    estilos de formulario

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./estilos_para_formulario/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "400px",
+      md: "768px",
+      lg: "960px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
